19bf6c39f323d9c75bdc2fe1079ea11352b40d3a
Onkar Apte <97520729+onapte@users.noreply.github.com>
2024-11-01 09:38:58 -0600

    Update trie.h

--- trie.h
+++ trie.h
@@ -5,23 +5,23 @@
 #include <iostream>
 #include <fstream>
 
-class TrieNode {
+class trienode {
 public:
-    TrieNode *children[26] = {};
+    trienode *children[26] = {};
     bool isTerminal = false;
 };
 
-class Trie {
-    TrieNode *root;
+class trie {
+    trienode *root;
 public:
-    Trie();
+    trie();
     void insert(const std::string &word);
     void autoComplete(const std::string &key);
 private:
-    void autocomplete(TrieNode *node, const std::string &prefix);
+    void autocomplete(trienode *node, const std::string &prefix);
 };
 
-void loadWords(Trie &t, const std::string &filename);
+void loadWords(trie &t, const std::string &filename);
 void saveWord(const std::string &filename, const std::string &word);
 
 #endif
